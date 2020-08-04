10092035d6a6f925fe68f6f63a0567c9368e61eb
Isaac Schmidt <ischmidt20@berkeley.edu>
2020-08-03 18:51:57 -0700

    replace time_of_game with start_time

--- cbbBot.py
+++ cbbBot.py
@@ -161,12 +161,12 @@ for game in games:
     game = game.replace('\n', '')
     if game not in games_over:
         print('Getting game info for ' + game + '..... ' + str(datetime.datetime.now(tz)))
-        try:
+        if True:
             game_info = get_info(game)
             (away_rank, away_team, away_record, home_rank, home_team, home_record, venue, city, state, network, start_time, away_flair, home_flair, game_clock, away_score, home_score) = game_info
             print('Obtained game info for ' + game + '! ' + str(datetime.datetime.now(tz)))
             if game not in already_posted.keys():
-                if datetime.datetime.now(tz) > (time_of_game - datetime.timedelta(minutes = 60)) and 'FINAL' not in game_clock and game not in blacklist: #if time is later than 60 minutes before game time, and game is not over, post thread, write thread_id to file
+                if datetime.datetime.now(tz) > (start_time - datetime.timedelta(minutes = 60)) and 'FINAL' not in game_clock and game not in blacklist: #if time is later than 60 minutes before game time, and game is not over, post thread, write thread_id to file
                     print('Posting game ' + game + ' ..... ' + str(datetime.datetime.now(tz)))
                     try:
                         (title, thread_text) = make_thread(game, game_info)
@@ -178,7 +178,7 @@ for game in games:
                 else:
                     print('Game ' + game + ' will not be posted at this time. ' + str(datetime.datetime.now(tz)))
             else:
-                if datetime.datetime.now(tz) > (time_of_game + datetime.timedelta(hours = 4)):
+                if datetime.datetime.now(tz) > (start_time + datetime.timedelta(hours = 4)):
                     with open('./data/games_over.txt', 'a') as f:
                         f.write(game + '\n')
                 try:
@@ -189,5 +189,5 @@ for game in games:
                     print('Edited thread ' + game + '! ' + str(datetime.datetime.now(tz)))
                 except:
                     print('Failed to edit thread ' + game + '. Will continue..... ' + str(datetime.datetime.now(tz)))
-        except:
+        else:
             print('Failed to get game info for ' + game + '. ' + str(datetime.datetime.now(tz)))
