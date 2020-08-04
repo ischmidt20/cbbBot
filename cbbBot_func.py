d1134b2953bbf2783e07b8c5cc8f881ffa0195fe
Isaac Schmidt <ischmidt20@berkeley.edu>
2020-08-03 17:41:30 -0700

    rewrite espn function to use requests

--- cbbBot_func.py
+++ cbbBot_func.py
@@ -2,6 +2,7 @@
 import requests
 import datetime
 import pytz
+import json
 
 tz = pytz.timezone('US/Eastern')
 
@@ -44,106 +45,30 @@ def get_rcbb_rank():
         for team in ranking:
             f.write(team + '\n')
 
+def if_exists(dct, key, value):
+    if key in dct:
+        return dct[key]
+    return value
+
 def espn(game_id):
-  url='http://www.espn.com/mens-college-basketball/game?gameId='+game_id
-  urllib.request.urlretrieve(url,'cbbBot/'+game_id+'.html')
-  with open('cbbBot/'+game_id+'.html','r') as imp_file:
-    lines=imp_file.readlines()
-  for line in lines:
-    if '<div id="custom-nav"' in line:
-      info=line
-      break
-  at_begin=info.find('<span class="long-name">')
-  at_end=info.find('<',at_begin+1)
-  away_team=info[at_begin+24:at_end]
+    url = 'http://site.api.espn.com/apis/site/v2/sports/basketball/mens-college-basketball/summary?event=' + str(game_id)
+    obj = requests.get(url)
+    game = json.loads(obj.content)
+    teams = game['header']['competitions'][0]['competitors']
 
-  home_info=info[at_end:]
-  ht_begin=home_info.find('<span class="long-name">')
-  ht_end=home_info.find('<',ht_begin+1)
-  home_team=home_info[ht_begin+24:ht_end]
+    (home_rank, away_rank) = [if_exists(team, 'rank', '') for team in teams]
+    (home_team, away_team) = [team['team']['location'] for team in teams]
+    (home_record, away_record) = [team['record'][0]['summary'] for team in teams]
+    (home_score, away_score) = [int(team['score']) for team in teams]
 
-  #away_team,home_team=similar(away_team),similar(home_team)
-  if info.find('<span class="rank">')==-1:
-    away_rank,home_rank='',''
-  else:
-    if info.find('<span class="rank">')>at_begin:
-      hrk_begin=info.find('<span class="rank">')
-      hrk_end=info.find('<',hrk_begin+1)
-      home_rank=info[hrk_begin+19:hrk_end]
-      away_rank=''
-    if info.find('<span class="rank">')<at_begin:
-      ark_begin=info.find('<span class="rank">')
-      ark_end=info.find('<',ark_begin+1)
-      away_rank=info[ark_begin+19:ark_end]
-      home_info=info[ark_end:]
-      if home_info.find('<span class="rank">')==-1:
-        home_rank=''
-      else:
-        hrk_begin=home_info.find('<span class="rank">')
-        hrk_end=home_info.find('<',hrk_begin+1)
-        home_rank=home_info[hrk_begin+19:hrk_end]
-  if away_rank!='':
-    away_rank='#'+away_rank
-  if home_rank!='':
-    home_rank='#'+home_rank
+    venue = game['gameInfo']['venue']['fullName']
+    city = game['gameInfo']['venue']['address']['city']
+    state = game['gameInfo']['venue']['address']['state']
+    network = ''
+    if len(game['header']['competitions'][0]['broadcasts']) > 0:
+        network = game['header']['competitions'][0]['broadcasts'][0]['media']['shortName']
 
-  arc_begin=info.find('<div class="record">')
-  arc_end=info.find('<',arc_begin+1) #fix
-  away_record=info[arc_begin+20:arc_end]
-  home_info=info[arc_end:]
-  hrc_begin=home_info.find('<div class="record">')
-  hrc_end=home_info.find('<',hrc_begin+1) #fix
-  home_record=home_info[hrc_begin+20:hrc_end]
+    start_time = pytz.utc.localize(datetime.datetime.strptime(game['header']['competitions'][0]['date'],'%Y-%m-%dT%H:%MZ')).astimezone(tz)
+    game_clock = game['header']['competitions'][0]['status']['type']['detail'] #check this pointer works during live action
 
-  lookup=False
-  venue,network='',''
-  for line in lines:
-    if '<h1>Game Information</h1>' in line:
-      lookup=True
-    if lookup:
-      if '<figure>' in line:
-        venue=lines[lines.index(line)+4].replace('\t','').replace('\n','')
-      else:
-        if '<div class="location-details">' in line and venue=='':
-          venue=lines[lines.index(line)+1][9:-8]
-      if '<li class="icon-font-before icon-location-solid-before">' in line:
-        city_state=lines[lines.index(line)+1].replace('\t','').replace('\n','')
-      if '<div class="game-network">' in line:
-        network=lines[lines.index(line)+1].replace('\t','').replace('\n','').replace('Coverage: ','')
-      if '<span data-date="' in line:
-        time_of_game=line[22:39]
-
-    if '</article>' in line:
-      lookup=False
-
-  time_of_game=pytz.utc.localize(datetime.datetime.strptime(time_of_game,'%Y-%m-%dT%H:%MZ')).astimezone(tz)
-
-  game_time=''
-  for line in lines:
-    if 'class="game-time' in line:
-      gt_begin=line.find('>',line.find('<span class="game-time'))
-      gt_end=line.find('</span>',gt_begin)
-      game_time=line[gt_begin+1:gt_end]
-      game_time=game_time.replace('<span class="status-detail">','')
-      break
-  game_time=game_time.replace(' Half','').replace(' - ',' ').upper()
-
-  away_score,home_score='',''
-  for line in lines:
-    if '<div class="score icon-font-' in line:
-      line=line.replace('before','').replace('after','')
-      as_begin=line.find('<div class="score icon-font-')
-      as_end=line.find('</div>',as_begin)
-      away_score=line[as_begin+30:as_end]
-      if away_score!='':
-        away_score=int(away_score)
-      line=line[as_end:]
-      hs_begin=line.find('<div class="score icon-font-')
-      hs_end=line.find('</div>',hs_begin)
-      home_score=line[hs_begin+30:hs_end]
-      if home_score!='':
-        home_score=int(home_score)
-      break
-
-  os.remove('cbbBot/'+game_id+'.html')
-  return away_rank,away_team,away_record,home_rank,home_team,home_record,venue,city_state,network,time_of_game,game_time,away_score,home_score
+    return away_rank,away_team,away_record,home_rank,home_team,home_record,venue,city,state,network,start_time,game_clock,away_score,home_score
--- cbbBot_newday.py
+++ cbbBot_newday.py
@@ -16,7 +16,7 @@ if now.day < 10:
 
 #year,month,day='2020','03','10'
 
-url = 'http://site.api.espn.com/apis/site/v2/sports/basketball/mens-college-basketball/scoreboard?dates='+year+month+day+'&groups=50&limit=353'
+url = 'http://site.api.espn.com/apis/site/v2/sports/basketball/mens-college-basketball/scoreboard?dates=' + year + month + day + '&groups=50&limit=353'
 obj = requests.get(url)
 schedule = json.loads(obj.content)
 
